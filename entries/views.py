f5d6c7cc4d2b7f6ef11d9078608ae57cfa6638f5
Hussein Shahab <hussein.a.shahab@gmail.com>
2022-06-07 23:17:33 +0300

    Step 7: Putting a Lock on Your Django Diary

--- entries/views.py
+++ entries/views.py
@@ -1,4 +1,6 @@
-#from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -20,15 +22,17 @@ class EntryDetailView(DetailView):
     model = Entry
 
 
-class EntryCreateView(CreateView):
+class EntryCreateView(SuccessMessageMixin, CreateView):
     model = Entry
     fields = ["title", "content"]
     success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
 
 
-class EntryUpdateView(UpdateView):
+class EntryUpdateView(SuccessMessageMixin, UpdateView):
     model = Entry
-    field = ["title", "content"]
+    fields = ["title", "content"]
+    success_message = "Your entry was updated!"
 
     def get_success_url(self):
         return reverse_lazy(
@@ -40,3 +44,8 @@ class EntryUpdateView(UpdateView):
 class EntryDeleteView(DeleteView):
     model = Entry
     success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
